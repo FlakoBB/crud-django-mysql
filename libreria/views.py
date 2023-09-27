698abc981d21869e74cb5b2383cbbd2a05056b0a
FlakoBB <josephrbb99@gmail.com>
2023-09-26 18:01:30 -0600

    vistas | direccionamiento | templates

--- libreria/views.py
+++ libreria/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+  return render(request, 'paginas/inicio.html')
+
+def nosotros(request):
+  return render(request, 'paginas/nosotros.html')
+
+def libros(request):
+  return render(request, 'libros/index.html')
+
+def crear(request):
+  return render(request, 'libros/crear.html')
+
+def editar(request):
+  return render(request, 'libros/editar.html')
