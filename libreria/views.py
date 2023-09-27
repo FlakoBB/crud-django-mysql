61083e6abbd9dd65ecf161a617eac8b050e13eea
FlakoBB <josephrbb99@gmail.com>
2023-09-27 14:37:01 -0600

    CRUD desde las vistas

--- libreria/views.py
+++ libreria/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Libro
+from .forms import LibroForm
 
 def inicio(request):
   return render(request, 'paginas/inicio.html')
@@ -7,10 +9,26 @@ def nosotros(request):
   return render(request, 'paginas/nosotros.html')
 
 def libros(request):
-  return render(request, 'libros/index.html')
+  # * obtener info de la base de datos
+  libros = Libro.objects.all()
+  return render(request, 'libros/index.html', {'libros': libros})
 
 def crear(request):
-  return render(request, 'libros/crear.html')
+  formulario = LibroForm(request.POST or None, request.FILES or None)
+  if formulario.is_valid():
+    formulario.save() # ? guarda la informacion obtenida del formulario
+    return redirect('libros') # ? redirecciona a la pagina '/libros'
+  return render(request, 'libros/crear.html', {'formulario': formulario})
 
-def editar(request):
-  return render(request, 'libros/editar.html')
+def editar(request, id):
+  libro = Libro.objects.get(id=id)
+  formulario = LibroForm(request.POST or None, request.FILES or None, instance=libro)
+  if formulario.is_valid() and request.POST:
+    formulario.save()
+    return redirect('libros')
+  return render(request, 'libros/editar.html', {'formulario': formulario})
+
+def eliminar(request, id):
+  libro = Libro.objects.get(id=id)
+  libro.delete()
+  return redirect('libros')
